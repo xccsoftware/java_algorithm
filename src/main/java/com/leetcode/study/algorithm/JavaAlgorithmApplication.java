3925cefc70549143bb8720cc1aaec3077a5a69e2
xcc <2269861418@qq.com>
2023-05-06 17:38:52 +0800

    初始版本

---
+++ src/main/java/com/leetcode/study/algorithm/JavaAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.leetcode.study.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAlgorithmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leetcode/study/algorithm/JavaAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcode.study.algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
